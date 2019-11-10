13636cd08814ed083c0d2bf4242b1dd3a3b0ef08
Rostyslav Druzhchenko <rostyslav.druzhchenko@gmail.com>
2019-11-10 13:02:25 +0200

    Create a `Starter` project for Part 2

---
+++ 201911-StaticLibP2/Starter/Networking/Networking/Networking.swift
@@ -0,0 +1,11 @@
+//
+//  Networking.swift
+//  Networking
+//
+//  Created by Rostyslav Druzhchenko on 10/9/19.
+//  Copyright © 2019 Rostyslav Druzhchenko. All rights reserved.
+//
+
+class Networking {
+
+}
